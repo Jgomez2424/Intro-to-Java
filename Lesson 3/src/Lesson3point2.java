d2576339577ae767572dce6a0197fb4632675800
joseph gomez <jgomez@HS-D210-06.MOHS.local>
2019-03-08 11:53:28 -0500

    Work on weekend!

--- Lesson 3/src/Lesson3point2.java
+++ Lesson 3/src/Lesson3point2.java
@@ -5,15 +5,15 @@ public class Lesson3point2 {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("a\\b\\c" + "''d''");
+		System.out.println("a\\b\\c" + "\"d\"");
 		System.out.println();
-		System.out.println("\\t is a tab, \\n is a new line, \\' prints a " + "'quote'");
+		System.out.println("\\t is a tab, \\n is a new line, \\\" prints a " + "\"quote\"");
 		System.out.println();
-		System.out.println("''line one''\n " + "\\line" + "                two\\");
+		System.out.println("\"line one\"\n " + "\\line" + "                two\\");
 		System.out.println();
 		System.out.println("\\\\denotes a single line comment\n" + "\\*denotes a multiline comment*\\");
 		System.out.println();
-		System.out.println("''\\\\''\\''''\n" + "                 \\''\\''''\\\n" + "\\''''''''\\"  );
+		System.out.println("\"\\\\\"\\\"\"\n" + "                 \\\"\\\"\"\\\n" + "\\\"\"\"\"\\"  );
 		System.out.println();
 		System.out.println();
 		
@@ -22,13 +22,14 @@ public class Lesson3point2 {
 		System.out.println();
 		
 		word = "it's my party";
-		System.out.println(word.substring(0,4) + " a " + "''PARTY''");
+		System.out.println(word.substring(0,4) + " a " + "\"PARTY\"");
 		System.out.println();
 		
 		word = "whose a whats it";
 		String whose = word.substring(0,5);
-		System.out.println(whose.toUpperCase());
-		
+		String hello = word.substring(7,12);
+		System.out.println(whose.toUpperCase() + "\n" + "is" + hello.toUpperCase());
+		System.out.println();
 		
 		word = "Every Way The Wind Blows";
 		String e = word.substring(0,5);
@@ -38,7 +39,39 @@ public class Lesson3point2 {
 		String b = word.substring(19,24);
 		String num = word.substring(0,4);
 		System.out.println(e.toLowerCase() + " " + q.toUpperCase() + " " + w.toLowerCase() + " \\" + o.toUpperCase() + "\\ " + b.toLowerCase() + " " + num.length());
-
+		System.out.println();
+		
+		word = "Peter Piper Picked A Peck Of Picked Peppers ";
+		String a = word.substring(19,20);
+		String v = word.substring(20,35);
+		String r = word.substring(11,18);
+		String find = word.substring(35,44);
+		String good = word.substring(0,12);
+		System.out.println(a + v.toLowerCase() + "          " + find.toLowerCase() + good + "        " + r.toLowerCase()  );
+		System.out.println();
+		
+		
+		word = "word has 4 letters";
+		String test = word.substring(9,10);
+		String eee = word.substring(11,18);
+		String ff = word.substring(4,8);
+		System.out.println("\"" + test + "\" " + eee + ff + " \"9\" " + eee);
+		System.out.println();
+		
+		word = "Jim, Jacky, Johnny, Jill";
+		String ww = word.substring(0,3);
+		String qq = word.substring(5,10);
+		String hh = word.substring(12,18);
+		String cc = word.substring(20);
+		
+		System.out.println(ww.toLowerCase() + "\\" + ww.length() + "\n" + qq.toUpperCase() + "\\" + + qq.length() + "\n" + hh.toLowerCase() + "\\" + hh.length() + "\n" + cc.toUpperCase() + "\\" + cc.length());
+		System.out.println();
+		
+		
+		
+		
+		
+		
 		
 		
 		
---
+++ Lesson 4.1 - Numeric Variables/src/Lesson_4.java
@@ -0,0 +1,73 @@
+
+public class Lesson_4 {
+
+	public static void main(String[] args) {
+
+		int h = 103;
+		int p = 5;
+		System.out.println(++h + p);
+		System.out.println(h);
+		System.out.println();
+		
+		
+		int j = 4;
+		System.out.println(j++);
+		System.out.println(j);
+		System.out.println(++j);
+		System.out.println(++j);
+		System.out.println();
+		
+		double def;
+		double f = 1992.37;
+		def = f;
+		System.out.println(def);
+		System.out.println();
+		
+		int zulu = 5;
+		System.out.println(--zulu);
+		System.out.println();
+		
+		int a = 100;
+		int b = 200;
+		b/=a;
+		System.out.println(b + 1);
+		System.out.println();
+		
+		int o = 40;
+		int v = o-=30;
+		System.out.println(v);
+		System.out.println();
+		
+		int Q = 40;
+		int E = Q+=10;
+		System.out.println(E);
+		System.out.println();
+		
+		int R = 40;
+		int q = 4;
+		System.out.println(2 + 8 * q/2 - R);
+		System.out.println();
+		
+		int sd = 12;
+		int x = 4;
+		System.out.println(sd%(++x));
+		System.out.println();
+		
+		int g;
+		g = 3;
+		System.out.println(++g*79);
+		System.out.println();
+		
+		double m = 1.0;
+		double B = 2.29;
+		double F = 9.0;
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
