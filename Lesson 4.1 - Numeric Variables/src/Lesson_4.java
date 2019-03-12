4edd5510ea8d41b074bbd35330f5686b97935a64
joseph gomez <jgomez@HS-D210-06.MOHS.local>
2019-03-12 14:12:26 -0400

    Test

--- Lesson 4.1 - Numeric Variables/src/Lesson_4.java
+++ Lesson 4.1 - Numeric Variables/src/Lesson_4.java
@@ -3,71 +3,43 @@ public class Lesson_4 {
 
 	public static void main(String[] args) {
 
-		int h = 103;
-		int p = 5;
-		System.out.println(++h + p);
-		System.out.println(h);
-		System.out.println();
-		
-		
-		int j = 4;
-		System.out.println(j++);
-		System.out.println(j);
-		System.out.println(++j);
-		System.out.println(++j);
-		System.out.println();
-		
-		double def;
-		double f = 1992.37;
-		def = f;
-		System.out.println(def);
-		System.out.println();
-		
-		int zulu = 5;
-		System.out.println(--zulu);
-		System.out.println();
-		
-		int a = 100;
-		int b = 200;
-		b/=a;
-		System.out.println(b + 1);
-		System.out.println();
-		
-		int o = 40;
-		int v = o-=30;
-		System.out.println(v);
-		System.out.println();
-		
-		int Q = 40;
-		int E = Q+=10;
-		System.out.println(E);
-		System.out.println();
-		
-		int R = 40;
-		int q = 4;
-		System.out.println(2 + 8 * q/2 - R);
-		System.out.println();
-		
-		int sd = 12;
-		int x = 4;
-		System.out.println(sd%(++x));
-		System.out.println();
-		
-		int g;
-		g = 3;
-		System.out.println(++g*79);
-		System.out.println();
-		
-		double m = 1.0;
-		double B = 2.29;
-		double F = 9.0;
+	int j = 79;
+	int W = 4;
+	int A = 21;
+	int a = 10;
+	int H = 10389;
+	int N = 82;
+	int q = 3;
+	int M = 68;
+	int Q = 7;
+	int w = 19;
+	int floor = 2246;
+	int Ten = 56;	
+	int ele = 11;
+	
+	
+		System.out.println(("79 + 3 * (4 + 82 - 68) - 7 + 19 ") +  "= " + (j + q * (W + N - M) - Q + w));
+		System.out.println(("(179 + 21 + 10) / 7 + 181 = ") + ((100 + j + A + a) / Q + 100 + N - 1));
+		System.out.println(("10389 * 56 * 11 + 2246 = ") + (H * Ten * ele + floor));
 		
 		
 		
 		
 		
 		
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
