d3f1a7cd0e294615ab059ff8e5eac1bb8dadec46
joseph gomez <jgomez@HS-D210-06.MOHS.local>
2019-03-13 13:19:13 -0400

    COMMIIIIIIGUDFUIGUHOUIHOIUFSHL

--- Lesson 5/src/Lesson5.java
+++ Lesson 5/src/Lesson5.java
@@ -3,30 +3,36 @@ public class Lesson5 {
 
 	public static void main(String[] args) {
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		double d1 = 37.9;
+		double d2 = 1004.128;
+		int i1 = 12;
+		int i2 = 18;
 		
 
+		System.out.println(57.2 * ((double) i1 / i2) + 1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
