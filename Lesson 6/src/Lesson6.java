a246dbeea3e75dacb1a59979fd420a9763e5aa9a
joseph gomez <jgomez@HS-D210-06.MOHS.local>
2019-03-14 12:11:02 -0400

    Work on this at home

---
+++ Lesson 6/src/Lesson6.java
@@ -0,0 +1,34 @@
+
+public class Lesson6 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		double loggers = 18;
+		
+		System.out.println(Math.log (loggers));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
